054fce0eb1450818605223c2c2b897402de84c1f
Stephan Raaijmakers <stephan@vmi2768320.contaboserver.net>
2025-12-11 23:18:29 +0100

    Update

--- code/week11/mcnemar.py
+++ code/week11/mcnemar.py
@@ -1,45 +1,60 @@
+import sys
+import re
 from statsmodels.stats.contingency_tables import mcnemar
-import random
 
-def make_contingency_table(results_1, results_2, ground_truth, alpha=.05):
-    assert len(results_1)==len(results_2)==len(ground_truth),"Error: incompatible sizes."
-    Results={}
-    Results[(1,1)]=0 # classifier 1 right, classifier 2 wrong (etc)
-    Results[(0,1)]=0
-    Results[(1,0)]=0
-    Results[(0,0)]=0
-    performance_1=0
-    performance_2=0
+# Input: a file with on every line one of M, N, MN, 00 (+ newline)
+# M: classifier 1 correct, classifier 2 wrong
+# N: classifier 1 wrong, classifier 2 right
+# MN: both correct
+# 00: both wrong
 
-    nb_observations=len(results_1)
-    for i in range(nb_observations):
-        score_1=0
-        score_2=0
-        if results_1[i]==ground_truth[i]:
-            score_1=1
-            performance_1+=1
-        if results_2[i]==ground_truth[i]:
-            score_2=1
-            performance_2+=1
-        Results[(score_1,score_2)]+=1
-    # see https://en.wikipedia.org/wiki/McNemar%27s_test
-    table=[[Results[(1,1)],Results[(1,0)]],
-           [Results[(0,1)],Results[(0,0)]]]
-    m = mcnemar(table, exact=False) # Use False for large samples, and True for small samples (<100)
-    # See https://www.statsmodels.org/stable/generated/statsmodels.stats.contingency_tables.mcnemar.html
+# Run: !python mcnemar.py <file>
 
-    print('McNemar statistic=%.6f, p-value=%.3f' % (m.statistic, m.pvalue))
-    
-    if m.pvalue > alpha:
-        print("Failure to reject H0. No significant difference between classifier A and classifier B.")
+
+
+def main(fn):
+    fp=open(fn,"r")
+    N=0
+    P_M=0.0
+    P_N=0.0
+    P_MN=0.0
+    P_None=0.0
+    for line in fp:
+        line=line.rstrip()
+        m=re.match("^([MN0]+)\s*$",line)
+        if m:
+            N+=1
+            tag=m.group(1)
+            if tag=="M":
+                P_M+=1
+            elif tag=='N':
+                P_N+=1
+            elif tag=="MN":
+                P_MN+=1
+            elif tag=="00": # both wrong
+                P_None+=1
+
+    #print(P_MN,P_M,P_N,P_None)
+
+    # define contingency table
+    table = [[P_MN,P_M],
+         [P_N,P_None]]
+    # calculate mcnemar test
+    result = mcnemar(table, exact=True)
+    # summarize the finding
+    print('statistic=%.3f, p-value=%f' % (result.statistic, result.pvalue))
+    # interpret the p-value
+    alpha = 0.05
+    if result.pvalue > alpha:
+        print('Same proportions of errors (fail to reject H0; p>.05)')
     else:
-        print("H0 rejected. Significant difference between classifier A and classifier B.")
-        if performance_1>performance_2:
-            print("A is better than B")
+        print('Different proportions of errors (reject H0; p<.05)')
+        if P_M>P_N:
+            print("Metadata better than no metadata")
         else:
-            print("B is better than A")
+            print("No metadata better than metadata")
+    fp.close()
+    exit(0)
 
-classifier_1=random.sample(range(0,100),100)
-classifier_2=random.sample(range(0,100),100)
-gt=list(range(100))
-make_contingency_table(classifier_1,classifier_2,gt)
+if __name__=="__main__":
+    main(sys.argv[1])
