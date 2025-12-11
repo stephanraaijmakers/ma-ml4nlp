68cc524a1beb78951fd6c39bc81da3189b61ae7c
Stephan Raaijmakers <stephan@vmi2768320.contaboserver.net>
2025-12-11 23:30:36 +0100

    Update

--- code/week11/mcnemar.py
+++ code/week11/mcnemar.py
@@ -49,6 +49,11 @@ def main(fn):
         print('Same proportions of errors (fail to reject H0; p>.05)')
     else:
         print('Different proportions of errors (reject H0; p<.05)')
+        if P_M>P_N:
+            print('Classifier 1 better.')
+        else:
+            print('Classifier 2 better.')
+            
     fp.close()
     exit(0)
 
